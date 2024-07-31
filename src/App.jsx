1db02767f37c0652c9864f34b4595cc8349a9486
Antoine <1363491@cstj.qc.ca>
2024-07-31 15:26:27 -0400

    ajoute scss package

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './components/nav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+
+      <Nav/>
+
+      <div className="mainCard">
+        ef
+      </div>
+
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +35,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ src/components/nav.jsx
@@ -0,0 +1,16 @@
+import "./navStyle.scss";
+
+function Nav() {
+  
+    return (
+      <>
+        
+        <nav className="topNav">
+          effewfe
+        </nav>
+
+      </>
+    )
+  }
+  
+  export default Nav
