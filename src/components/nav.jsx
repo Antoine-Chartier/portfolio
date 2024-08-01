5b318986cd6925baa25c33d77091183bb4b32512
Antoine <1363491@cstj.qc.ca>
2024-08-01 13:11:23 -0400

    hamburger animé

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import "./navStyle.scss";
+import { motion } from "framer-motion";
+
 
 function Nav() {
 
@@ -22,7 +25,7 @@ function Nav() {
                 </div>
              </div>
              <div className="button">
-
+                <AnimatedHamburgerButton />
              </div>
              <div className="mobileMenu hidden">
                 <div>
@@ -42,3 +45,49 @@ function Nav() {
   }
   
   export default Nav
+
+
+  const AnimatedHamburgerButton = () => {
+    const [active, setActive] = useState(false);
+    return (
+      <motion.button 
+      onClick={() => setActive(!active)}
+      animate={active ? "open" : "closed"}
+      className="buttonHamburger">
+        <motion.span
+          variants={{
+            open: {rotate: "45deg"},
+            closed: {rotate: "0deg"}
+          }}
+          style={{
+              left: "33%",
+              top: "30%",
+              x: "-50%",
+              y: "-50%",
+            }} className="barre1"/>
+        <motion.span
+          variants={{
+            open: {rotate: "-45deg"},
+            closed: {rotate: "0deg"}
+          }}
+          transition={{duration: 0.2, ease: "easeInOut"}}
+          style={{
+            left: "50%",
+            top: "50%",
+            x: "-50%",
+            y: "-50%",
+          }} className="barre2"/>
+        <motion.span 
+          variants={{
+            open: {rotate: "45deg"},
+            closed: {rotate: "0deg"}
+          }}
+          style={{
+            left: "66%",
+            top: "70%",
+            x: "-50%",
+            y: "-50%",
+          }} className="barre3"/>
+      </motion.button>
+    );
+  }
