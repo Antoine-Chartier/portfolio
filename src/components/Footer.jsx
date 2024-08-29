d001fb3c99549c4d54240ba108b9dcf6c9ecc622
Antoine <1363491@cstj.qc.ca>
2024-08-29 16:59:04 -0400

    color system et footer termine

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,32 @@
 import "./Footer.scss";
+import { CiLinkedin } from "react-icons/ci";
+import { FiGithub } from "react-icons/fi";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer>
-      <p>© 2021 Your Name</p>
+      <div className="coordonnees">
+        <div>
+          <h3>Tel</h3>
+          <p><a href="tel://5143781179">(514) 378-1179</a></p>
+        </div>
+        <div>
+          <h3>E-mail</h3>
+          <p><a href="mailto:antoinechartier.ca@gmail.com?subject=Offre%20d'emploi&body=Bonjour%20Antoine,">antoinechartier.ca@gmail.com</a></p>
+        </div>
+        <div>
+          <h3>Réseaux</h3>
+          <div className="icones">
+            <CiLinkedin className="icone" />
+            <FiGithub className="icone" />
+          </div>
+        </div>
+      </div>
+      <div className="copyright">
+        <p>© {currentYear} Antoine Chartier</p>
+      </div>
     </footer>
   );
 };
