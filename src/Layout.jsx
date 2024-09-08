34a4dd379f0daeb60b9f711cbcc20cf6a81f0dd1
Antoine <1363491@cstj.qc.ca>
2024-09-08 19:37:27 -0400

    test sans nav

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom";
-
 import Footer from "./components/Footer";
-import Nav from "./components/Nav.jsx";
 
 const Layout = () => {
 
   return (
     <div>
       <div className="background" style={{ position: "fixed", width: "100%" }}>
-        <Nav />
+        {/* <Nav /> */}
       </div>
       <div className="menuTampon">
         <div>{/* menu zone */}</div>
