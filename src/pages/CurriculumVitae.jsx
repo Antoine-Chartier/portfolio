e70944035f34afd5bb6eb5d22d3ba4b332a52a87
Antoine <1363491@cstj.qc.ca>
2024-10-24 11:48:53 -0400

    test iframe

--- src/pages/CurriculumVitae.jsx
+++ src/pages/CurriculumVitae.jsx
@@ -1,8 +1,10 @@
 
+
 function CurriculumVitae() {
     return (
-        // <div>
-            <iframe src="src\assets\AntoineChartier-CV.pdf" width="100%" height="1000px"></iframe>
+        <div style={{height: "100vh"}}>
+            <iframe src="src\assets\AntoineChartier-CV.pdf" width="100%" style={{position:"relative", height:"100%", width:"100%", overflow:"auto"}}></iframe>
+         </div>   
             // testing CurriculumVitae
 
     );
