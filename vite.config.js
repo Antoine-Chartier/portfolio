26bc8440fd7e504da9e5cc9ccd9fe7b9168b2985
Antoine <1363491@cstj.qc.ca>
2024-10-26 01:20:22 -0400

    enleve copypublicdir

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { imageOptimizerPlugin } from './src/plugin/imagesOptimizerPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: { manifest: true},
+  build: {copyPublicDir: false, manifest: true},
   plugins: [ imageOptimizerPlugin(), react()],
 })
