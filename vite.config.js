d8f6c0dfa18d9ef07a7561c1e1d93fc4d9dec151
Antoine <1363491@cstj.qc.ca>
2024-10-26 01:14:15 -0400

    enelver chat, garde manifest

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import copy from 'rollup-plugin-copy';
-import { imageOptimizerPlugin } from "./src/plugin/imagesOptimizerPlugin";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { imageOptimizerPlugin } from './src/plugin/imagesOptimizerPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    imageOptimizerPlugin(),
-    react(),
-    copy({
-      targets: [{ src: "public/AntoineChartier-CV-pub.pdf", dest: "dist" }],
-      hook: "writeBundle",
-    }),
-  ],
-  build: {
-    build: {
-      rollupOptions: {
-        output: {
-          assetFileNames: (assetInfo) => {
-            if (assetInfo.name === "AntoineChartier-CV-pub.pdf") {
-              return "AntoineChartier-CV.pdf";
-            }
-            return assetInfo.name;
-          },
-        },
-        manifest: true,
-      },
-    },
-  },
-});
+  build: { manifest: true},
+  plugins: [ imageOptimizerPlugin(), react()],
+})
