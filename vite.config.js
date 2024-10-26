d6e3e35eee030a5e7899687e9f6022963c09a182
Antoine <1363491@cstj.qc.ca>
2024-10-26 12:17:59 -0400

    tes manifest default locatioon

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { imageOptimizerPlugin } from './src/plugin/imagesOptimizerPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {manifest: "manifest.json"},
+  build: {manifest: true},
   plugins: [ imageOptimizerPlugin(), react()],
 })
