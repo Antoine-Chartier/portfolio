7c72529377a8710f80f4abd708a35f4a130dda62
Antoine <1363491@cstj.qc.ca>
2024-10-26 00:28:41 -0400

    test syntax

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import { imageOptimizerPlugin } from './src/plugin/imagesOptimizerPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: [cssCodeSplit = false],
+  build: {cssCodeSplit: false},
   plugins: [ imageOptimizerPlugin(), react()],
 })
