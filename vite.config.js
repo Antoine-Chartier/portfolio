ba4b5821b3b040e88f6651609cce02f311ae511b
Antoine <1363491@cstj.qc.ca>
2024-10-26 12:30:33 -0400

    test plugin vite pour editer le manifest

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { imageOptimizerPlugin } from './src/plugin/imagesOptimizerPlugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { imageOptimizerPlugin } from "./src/plugin/imagesOptimizerPlugin";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {manifest: "manifest.json"},
-  plugins: [ imageOptimizerPlugin(), react()],
-})
+  build: { manifest: "manifest.json" },
+  plugins: [
+    imageOptimizerPlugin(),
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Mon Portfolio",
+        short_name: "Portfolio",
+        // start_url: "/custom-start-url",
+        display: "standalone",
+        background_color: "#ffffff",
+        description: "A portfolio built with Vite and React",
+        // icons: [
+        //   {
+        //     src: "/icons/icon-192x192.png",
+        //     sizes: "192x192",
+        //     type: "image/png",
+        //   },
+        //   {
+        //     src: "/icons/icon-512x512.png",
+        //     sizes: "512x512",
+        //     type: "image/png",
+        //   },
+        // ],
+      },
+    }),
+  ],
+});
