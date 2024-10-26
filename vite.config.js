9b560ac67706af9d5c0d7a1da4449c1949f0c553
Antoine <1363491@cstj.qc.ca>
2024-10-26 12:06:02 -0400

    test chemin config manifest

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { imageOptimizerPlugin } from './src/plugin/imagesOptimizerPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {manifest: true},
+  build: {manifest: "/manifest.json}",
   plugins: [ imageOptimizerPlugin(), react()],
 })
