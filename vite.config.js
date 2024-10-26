a43784febebcc524aafc112ce398bfcf5b5f0705
Antoine <1363491@cstj.qc.ca>
2024-10-26 01:04:04 -0400

    ajoute import copy plugin et manifest

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { imageOptimizerPlugin } from './src/plugin/imagesOptimizerPlugin'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import copy from 'rollup-plugin-copy';
+import { imageOptimizerPlugin } from "./src/plugin/imagesOptimizerPlugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ imageOptimizerPlugin(), react(), 
+  plugins: [
+    imageOptimizerPlugin(),
+    react(),
     copy({
-      targets: [
-        { src: 'public/AntoineChartier-CV-pub.pdf', dest: 'dist' }
-      ],
-      hook: 'writeBundle'
-    })],
+      targets: [{ src: "public/AntoineChartier-CV-pub.pdf", dest: "dist" }],
+      hook: "writeBundle",
+    }),
+  ],
+  build: {
     build: {
       rollupOptions: {
         output: {
           assetFileNames: (assetInfo) => {
-            if (assetInfo.names === 'AntoineChartier-CV-pub.pdf') {
-              return 'AntoineChartier-CV.pdf';
+            if (assetInfo.name === "AntoineChartier-CV-pub.pdf") {
+              return "AntoineChartier-CV.pdf";
             }
             return assetInfo.name;
-          }
-        }
-      }
-    }
-})
+          },
+        },
+        manifest: true,
+      },
+    },
+  },
+});
