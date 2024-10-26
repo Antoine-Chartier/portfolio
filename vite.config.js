855650b6f5cae36ac07887392b51c0b0739e31ae
Antoine <1363491@cstj.qc.ca>
2024-10-26 00:59:55 -0400

    test chat gpt

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,23 @@ import { imageOptimizerPlugin } from './src/plugin/imagesOptimizerPlugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {copyPublicDir: false},
-  plugins: [ imageOptimizerPlugin(), react()],
+  plugins: [ imageOptimizerPlugin(), react(), 
+    copy({
+      targets: [
+        { src: 'public/AntoineChartier-CV-pub.pdf', dest: 'dist' }
+      ],
+      hook: 'writeBundle'
+    })],
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.names === 'AntoineChartier-CV-pub.pdf') {
+              return 'AntoineChartier-CV.pdf';
+            }
+            return assetInfo.name;
+          }
+        }
+      }
+    }
 })
