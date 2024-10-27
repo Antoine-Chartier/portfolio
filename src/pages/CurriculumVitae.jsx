cad288568cea2deade7a8568ccf4439d2b140b26
Antoine <1363491@cstj.qc.ca>
2024-10-27 00:06:09 -0400

    test cv

--- src/pages/CurriculumVitae.jsx
+++ src/pages/CurriculumVitae.jsx
@@ -1,11 +1,8 @@
-
 import cvpdf from "/AntoineChartier-CV.pdf";
 
 function CurriculumVitae() {
-
-
   return (
-    <div style={{ height:"100%", overflow: "hidden" }}>
+    <div style={{ height: "100%", overflow: "hidden" }}>
       <iframe
         // id="pdfIframe"
         src={cvpdf}
@@ -15,8 +12,7 @@ function CurriculumVitae() {
         style={{ width: "100%", height: "100%", border: "none" }}
       />
 
-
-      <p style={{color:"red", padding:"var(--clampRacine)"}}>stuff</p>
+      <p style={{ color: "red", padding: "var(--clampRacine)" }}>stuff</p>
     </div>
   );
 }
