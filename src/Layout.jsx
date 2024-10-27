235bf4a0e90bb5d6a469a005606e7e7986238721
Antoine <1363491@cstj.qc.ca>
2024-10-27 16:08:42 -0400

    footer affiché dans cv seulement sur mobile

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-
+import { useState } from "react";
 
 const Layout = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 767);
+
+
+  // Define the paths where the Footer should not be rendered
+  const noFooterPaths = ["/cv"];
+
+  // Check if the current path is in the noFooterPaths array
+  const shouldNOTrenderFooter = noFooterPaths.includes(currentPath) && isDesktop;
+
   return (
     <>
       <Nav />
-      <main style={{ flex:"1 1 auto"}}>
+      <main style={{ flex: "1 1 auto" }}>
         <Outlet />
       </main>
-      <Footer />
+      {!shouldNOTrenderFooter && <Footer/>}
     </>
   );
 };
