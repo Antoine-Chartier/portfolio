933fc31fb7120da9e485ea94105fce9a2a49f181
Antoine <1363491@cstj.qc.ca>
2024-10-29 12:43:36 -0400

    test augmenter file size to cache

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       workbox: {
         // navigateFallbackDenylist: [/\.pdf$/],
         globPatterns: ['**/*.{js,css,html,ico,png,webp,avif,svg,pdf}'],
+        maximumFileSizeToCacheInBytes: 3000000,
       },
       manifest: {
         name: "Mon Portfolio",
