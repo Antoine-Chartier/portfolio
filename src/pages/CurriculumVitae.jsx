b8599f3bb1d8de71a1545805fe7b8e58ef739b62
Antoine <1363491@cstj.qc.ca>
2024-10-31 15:54:02 -0400

    test resize CV

--- src/pages/CurriculumVitae.jsx
+++ src/pages/CurriculumVitae.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import cvpdf from "/AntoineChartier-CV.pdf";
 
 function CurriculumVitae() {
+
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+console.log(screenSize);
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+
   return (
     <div style={{ height: "100%", overflow: "hidden" }}>
       <iframe
@@ -9,7 +20,7 @@ function CurriculumVitae() {
         type="application/pdf"
         width="100%"
         height="100%"
-        style={{ width: "100%", height: "100%", border: "none" }}
+        style={{ width: screenSize, height: "100%", border: "none" }}
       />
       <p style={{ color: "red", padding: "var(--clampRacine)" }}>stuff</p>
     </div>
