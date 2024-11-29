b77496e49b1a021b82c8bfb5715f1f0955879035
Antoine <1363491@cstj.qc.ca>
2024-11-29 13:40:10 -0500

    little app.jsx update syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.scss";
 import Layout from "./Layout";
 import Accueil from "./pages/Accueil";
@@ -10,26 +6,25 @@ import UnePage from "./pages/UnePage";
 import UneAutrePage from "./pages/UneAutrePage";
 import CurriculumVitae from "./pages/CurriculumVitae";
 
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { element: <Accueil />, index: true },
+      { path: "unepage", element: <UnePage /> },
+      { path: "uneAutrePage", element: <UneAutrePage /> },
+      { path: "CV", element: <CurriculumVitae /> },
+    ],
+  },
+  {
+    path: "*",
+    element: <Navigate to="/" replace />,
+  },
+]);
+
 function App() {
-
-  const routes = [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        { element: <Accueil />, index: true },
-        { path: "unepage", element: <UnePage /> },
-        { path: "uneAutrePage", element: <UneAutrePage /> },
-        { path: "CV", element: <CurriculumVitae /> },
-      ],
-    },
-    {
-      path: "*",
-      element: <Navigate to="/" replace />,
-    },
-  ];
-
-  return <RouterProvider router={createBrowserRouter(routes)} />;
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
