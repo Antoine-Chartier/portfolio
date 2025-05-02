e82ea5801361d8df8b0c1f6f03103adac37396a3
Antoine <antoinechartier.ca@gmail.com>
2025-05-02 16:26:09 -0400

    test onSubmit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,24 +4,30 @@ const Contact = () => {
   return (
     <div className="clamp">
       Je suis un form
-      <form name="contact" action="/succes" method="post" data-netlify="true"> {/* Updated action */}
-      <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-        {/* Outlet removed as Succes is no longer a child */}
+      <form name="contact" action="/succes/" method="post" data-netlify="true" onSubmit="submit">
+        <input type="hidden" name="form-name" value="contact" />
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+      {/* Outlet removed as Succes is no longer a child */}
     </div>
   );
-}
+};
 
 export default Contact;
